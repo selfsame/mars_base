610cd60ba8138dd009e4dad04e0b7a3af1b69748
ddmills <dmills@iastate.edu>
2013-06-06 10:38:23 -0500

    at least it works, it's still not what we need though

--- js/maptest.js
+++ js/maptest.js
@@ -1,84 +1,105 @@
-// get the window object
-var win = document.defaultView;
+$(window).ready( function(){
+	alert("window is ready");
+	
+	// get the window object
+	var win = document.defaultView;
 
-// Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 608;
-canvas.height = 512;
-document.body.appendChild(canvas);
-
-// tile object
-function tile(map, types, x, y) {
-	this.map = map;
-	this.types = type;
-	this.posx = x;
-	this.posy = y;
-	this.draw = draw;
-	this.getNeighbors = getNeighbors;
+	// Create the canvas
+	var canvas = document.createElement("canvas");
+	var ctx = canvas.getContext("2d");
+	canvas.width = 608;
+	canvas.height = 512;
+	document.body.appendChild(canvas);
 	
+	// collect the images
+	// eventually this somewhere else
+	win.images = {};
+	images.dirt = new Image();
+	images.dirt.src = "./textures/ground/dirt.png";
+	images.medical = new Image();
+	images.medical.src = "./textures/ground/room_medical.png";
+	images.corridor = new Image();
+	images.corridor.src = "./textures/ground/room_corridor.png";
+	images.rock = new Image();
+	images.rock.src = "./textures/objects/rock.png";
+	images.alpha = new Image();
+	images.alpha.src = "./textures/objects/alphasquare.png";
 	
-	
-	for (var i = 0; i < types.length; i++) {
-		var type = types[i];
-		var img = new Image();
-		if (type == "d") {
-			img.src = "./textures/ground/dirt.png";
-		} else if (type == "m") {
-			img.src = "./textures/ground/room_medical.png";
-		} else if (type == "c") {
-			img.src = "./textures/ground/room_corridor.png";
-		} else if (type == "r") {
-			img.src = "./textures/objects/rock.png";
-		} else if (type == "a") {
-			img.src = "./textures/objects/alphasquare.png";
-		}
-		this.draw(img);
-	}
-	
-	function draw(imgOb) {
-		ctx.drawImage(imgOb, this.posx * 32, this.posy * 32);
-	}
-	
-	function getNeighbors() {
-		return "derp";
-	}
-}
-
-// map object
-function map(mapName) {
-	this.tiles = [];
-	this.draw = draw;
-	this.mapName = mapName;
-	this.m = win.maps[mapName];
-	
-	this.width = this.m[0].length;
-	this.height = this.m.length;
-	
-	for (var i = 0; i < this.width; i++) {
-		var column = [];
-		for(var j = 0; j < this.height; j++) {
-			column[j] = new tile(this, this.m[j][i], i, j); 
-		}
-		this.tiles[i] = column;
-	}
-	
-	function draw() {
-		for (var i = 0; i < this.width; i++) {
-			for (var j = 0; j < this.height; j++) {
-				this.tiles[i][j].draw();
+	// tile object
+	function tile(map, types, x, y) {
+		this.map = map;
+		this.types = type;
+		this.posx = x;
+		this.posy = y;
+		this.draw = draw;
+		this.getNeighbors = getNeighbors;
+		
+		for (var i = 0; i < types.length; i++) {
+			var type = types[i];
+			if (type == "d") {
+				this.draw(images.dirt);
+			} else if (type == "m") {
+				this.draw(images.medical);
+			} else if (type == "c") {
+				this.draw(images.corridor);
+			} else if (type == "r") {
+				this.draw(images.rock);
+			} else if (type == "a") {
+				this.draw(images.alpha);
 			}
 		}
+		
+		function draw(imgOb) {
+			ctx.drawImage(imgOb, this.posx * 32, this.posy * 32);
+		}
+		
+		function getNeighbors() {
+			return "derp";
+		}
+	}
+
+	// map object
+	function map() {
+		this.tiles = [];
+		this.draw = draw;
+		this.load = load;
+		
+		this.width = 0;
+		this.height = 0;
+		
+		function load(mapName) {
+			this.mapName = mapName;
+			this.m = win.maps[mapName];
+			this.width = this.m[0].length;
+			this.height = this.m.length;
+			
+			
+			for (var i = 0; i < this.width; i++) {
+				var column = [];
+				for(var j = 0; j < this.height; j++) {
+					column[j] = new tile(this, this.m[j][i], i, j); 
+				}
+				this.tiles[i] = column;
+			}
+		}
+		
+		function draw() {
+			for (var i = 0; i < this.width; i++) {
+				for (var j = 0; j < this.height; j++) {
+					this.tiles[i][j].draw();
+				}
+			}
+		}
+		
+		function getTile(x, y) {
+			return this.tiles[x][y]
+		}
 	}
 	
-	function getTile(x, y) {
-		return this.tiles[x][y]
-	}
-}
-
-// create a map
-
-// display it
-var tileMap = new map("mars");
-$(window).ready( function(){tileMap.draw();} );
+	// now make the map
+	// eventually get rid of load and draw functions
+	var tileMap = new map();
+	tileMap.load("mars");
+	tileMap.draw();
+} );
 
