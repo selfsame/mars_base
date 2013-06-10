0f84f1b7eb123a0c1e65f039212382273f85cf42
ddmills <dmills@iastate.edu>
2013-06-10 18:11:11 -0500

    finally something productive!

--- js/room_manager.js
+++ js/room_manager.js
@@ -1,26 +1,177 @@
-function Tile(x, y, room, state) {
+function blueprint_tile(x, y, type, state) {
+	this.x = x;
+	this.y = y;
+	this.type = type;
+	this.state = state;
+	//this.tile = window.rooms.get_tile(x, y);
+	this.draw = draw;
+	this.toggle = toggle;
+	this.check_neighbors = check_neighbors;
+	this.set_state = set_state;
+	this.check_state = check_state;
+	this.check_obstacles = check_obstacles;
+	
+	function toggle() { // The player has clicked this tile while in edit mode
+	
+		// change the state based on what it is currently;
+		if (this.state == 0) {
+			this.type = window.Rooms.editType;
+			s = 1;
+		} else if (this.state == 1) {
+			s = 0;
+		} else if (this.state == 2) {
+			s = 0;
+		} else if (this.state == 3) {
+			s = 0;
+		} else if (this.state == 4) {
+			s = 4;
+		}
+
+		// check to make sure the state is ok
+		this.check_state(s);
+		
+	}
+	
+	function check_neighbors() { // Check all the neighboring states
+		n = window.Map.get_neighbors("blueprints", this.x, this.y);
+		tot = 0;
+		for (i = 0; i < n.length; i++) {
+			if (n[i].type == this.type) {
+				tot++;
+				old_state = n[i].state;
+				n[i].check_state(old_state);
+				//if (old_state != neighbors[i].state) {
+				//	neighbors[i].check_neighbors();
+				//}
+			}
+		}
+		//alert(this.n);
+		//alert(this.type + " total similiar neighbors: " + tot);
+		
+	}
+	
+	function set_state(new_state) { // change the tiles state and redraw
+		old_state = this.state;
+		if(new_state != old_state) {
+			this.state = new_state;
+			this.draw();
+		}
+	}
+	
+	function check_state(new_state) { // This function looks at neighboring tiles and it's current state, to figure out if it should change
+		
+		neighbors = window.Map.get_neighbors("blueprints", this.x, this.y);
+		
+		main = this;
+		function check(tile) {
+			return((tile.state == 1 || tile.state == 2 || tile.state == 4) && tile.type == main.type);
+		}
+		
+		//console.log(neighbors[0].type + " " + neighbors[0].state + " " + check(neighbors[0]) + " " + this.type + " " + ((neighbors[0].state == 1 || neighbors[0].state == 2 || neighbors[0].state == 4) && neighbors[0].type == this.type));
+		if (this.state == 0 && new_state == 0) {
+			return;
+		}
+		// if the state is "EMPTY" we need to make sure it doesn't affect neighbors
+		if (new_state == 0) {
+			this.set_state(0);
+			this.check_neighbors();
+			return;
+		}
+		//if the state is "BUILD" or "INVALID" you need to check for a square around it
+		if(new_state == 1 || new_state == 2) {
+		
+			found_square = false;
+			
+			// check top left
+			if (check(neighbors[0]) && check(neighbors[1]) && check(neighbors[7])) {
+				neighbors[0].set_state(1);
+				neighbors[1].set_state(1);
+				neighbors[7].set_state(1);
+				this.set_state(1);
+				found_square = true;
+			}
+			
+			// check top right
+			if (check(neighbors[1]) && check(neighbors[2]) && check(neighbors[3])) {
+				neighbors[1].set_state(1);
+				neighbors[2].set_state(1);
+				neighbors[3].set_state(1);
+				this.set_state(1);
+				found_square = true;
+			}
+			
+			// check bottom right
+			if (check(neighbors[3]) && check(neighbors[4]) && check(neighbors[5])) {
+				neighbors[3].set_state(1);
+				neighbors[4].set_state(1);
+				neighbors[5].set_state(1);
+				this.set_state(1);
+				found_square = true;
+			}
+			
+			// check bottom left
+			if (check(neighbors[7]) && check(neighbors[6]) && check(neighbors[5])) {
+				neighbors[7].set_state(1);
+				neighbors[6].set_state(1);
+				neighbors[5].set_state(1);
+				this.set_state(1);
+				found_square = true;
+			}
+			
+			// no square found, return invalid
+			//alert("no square found.");
+			if (!found_square) {
+				this.set_state(2);
+			}
+			return;
+			
+		} else {
+			this.set_state(2);
+			return;
+		}
+	
+	}
+	
+	function check_obstacles() {
+		// THIS IS WHERE IT CHECKS FOR OBSTACLES
+		return false;
+	}
+	
+	function draw() {
+		tilesize = window.Map.tilesize;
+		
+		if (this.state == 0) { // "EMPTY"
+			window.Draw.use_layer("blueprints");
+			window.Draw.clear_box(this.x * tilesize, this.y * tilesize, tilesize, tilesize);
+		} else if (this.state == 1) { // "BUILD"
+			window.Draw.use_layer("blueprints");
+			window.Draw.clear_box(this.x * tilesize, this.y * tilesize, tilesize, tilesize);
+			window.Draw.image(this.type, this.x * tilesize, this.y * tilesize);
+			window.Draw.image("blueprint1", this.x * tilesize, this.y * tilesize);
+		} else if (this.state == 2) { // "INVALID"
+			window.Draw.use_layer("blueprints");
+			window.Draw.clear_box(this.x * tilesize, this.y * tilesize, tilesize, tilesize);
+			window.Draw.image(this.type, this.x * tilesize, this.y * tilesize);
+			window.Draw.image("blueprint2", this.x * tilesize, this.y * tilesize);
+		} else if (this.state == 3) { // "REMOVE"
+			window.Draw.use_layer("blueprints");
+			window.Draw.clear_box(this.x * tilesize, this.y * tilesize, tilesize, tilesize);
+			window.Draw.image(this.type, this.x * tilesize, this.y * tilesize);
+			window.Draw.image("blueprint3", this.x * tilesize, this.y * tilesize);
+		} else if (this.state == 4) { // "BUILT"
+			window.Draw.use_layer("blueprints");
+			window.Draw.clear_box(this.x * tilesize, this.y * tilesize, tilesize, tilesize);
+			window.Draw.image(this.type, this.x * tilesize, this.y * tilesize);
+			window.Draw.image("blueprint4", this.x * tilesize, this.y * tilesize);
+		}
+	}	
+}
+function room_tile(x, y, room) {
 	this.x = x;
 	this.y = y;
 	this.room = room;
-	this.state = state;
 	this.draw = draw;
-	this.build = build;
-	this.update_state = update_state;
-	// build the tile
-	function build() {
-		this.state = 4;
-		this.draw();
-	}
-	
-	// figures out what the current state should be
-	function update_state() {
-		this.state = 4;
-		this.room.update_tile(this);
-		
-		this.draw();
-		return this.state; // move stuff over from window.Blueprints to here
-	}
-	
+
 	// display the tile
 	function draw() {
 		tilesize = window.Map.tilesize;
@@ -54,6 +205,7 @@ function Tile(x, y, room, state) {
 	}
 }
 
+/*
 function Room(type) {
 	this.type = type;
 	
@@ -104,6 +256,80 @@ function Room(type) {
 	function contains(tile) { // check if this room contains the given tile
 		return (this.tiles.build.indexOf(tile) > -1 && this.tiles.built.indexOf(tile) > -1);
 	} 
+}
+*/
+
+window.Rooms = {
+	init: function() {
+		
+		this.mode = "edit";
+		this.editType = "corridor";
+		
+		// ask for events
+		window.Events.add_listener(this);
+	
+		// load room images
+		window.Draw.add_image('medical', "./textures/ground/room_medical.png");
+		window.Draw.add_image('corridor', "./textures/ground/room_corridor.png");
+		window.Draw.add_image('laboratory', "./textures/ground/room_laboratory.png");
+		window.Draw.add_image('commons', "./textures/ground/room_commons.png");
+		window.Draw.add_image('greenhouse', "./textures/ground/room_greenhouse.png");
+		window.Draw.add_image('power', "./textures/ground/room_power.png");
+		window.Draw.add_image('supply', "./textures/ground/room_supply.png");
+		
+		// load blueprint images
+		window.Draw.add_image('blueprint1', "./textures/ground/blueprint_build.png");
+		window.Draw.add_image('blueprint2', "./textures/ground/blueprint_invalid.png");
+		window.Draw.add_image('blueprint3', "./textures/ground/blueprint_remove.png");
+		window.Draw.add_image('blueprint4', "./textures/ground/blueprint_built.png");
+		
+		// create draw layers for rooms and blueprints
+		window.Draw.create_layer("rooms", true);
+		window.Draw.create_layer("blueprints", true);
+		
+		// create map layers for blueprint and room tiles
+		room_tiles = [];
+		blueprint_tiles = [];
+		for (i = 0; i <= window.Map.height-1; i++) {
+			room_tiles.push([]);
+			blueprint_tiles.push([]);
+			for (j = 0; j <= window.Map.width-1; j++) {
+				rt = new room_tile(j, i, "outside", 5);
+				bt = new blueprint_tile(j, i, "empty", 0);
+				room_tiles[i].push(rt);
+				blueprint_tiles[i].push(bt);
+			}
+		}
+		window.Map.arrays["rooms"] = room_tiles;
+		window.Map.arrays["blueprints"] = blueprint_tiles;
+	},
+	mousedown: function(e){
+		if (this.mode == "edit") {
+			// get the tile the user clicked
+			tile_coords = window.Events.tile_under_mouse;
+			tile = window.Map.get("blueprints", tile_coords[0], tile_coords[1]);
+			tile.toggle();
+			
+		}
+	},
+	keydown: function(e){
+		if (e.keyCode == 49) { // 1
+			this.editType = "corridor";
+		} else if (e.keyCode == 50) { // 2
+			this.editType = "supply";
+		} else if (e.keyCode == 51) { // 3
+			this.editType = "greenhouse";
+		} else if (e.keyCode == 52) { // 4
+			this.editType = "commons";
+		} else if (e.keyCode == 53) { // 5
+			this.editType = "laboratory";
+		} else if (e.keyCode == 54) { // 6
+			this.editType = "medical";
+		} else if (e.keyCode == 55) { // 7
+			this.editType = "power";
+		}
+	}
+	
 }
 
 window.Blueprint = {
@@ -273,7 +499,7 @@ window.Blueprint = {
 	}
 };
 
-window.Rooms = {
+window.Rooms2 = {
 	init: function() {
 		this.rooms = [];
 		
